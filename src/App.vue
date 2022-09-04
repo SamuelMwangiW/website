cb7b146b7b20fec08b8790c05434d40a8c0d5c51
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-09-04 07:41:08 +0300

    Resize

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
 
 <template>
   <div class="flex items-center justify-center align-middle h-screen">
-    <div class="max-w-sm">
+    <div class="w-[480px] h-[240px]">
       <div class="coding inverse-toggle px-5 pt-4 shadow-lg text-gray-100 text-sm font-mono subpixel-antialiased
-              bg-gray-800  pb-6 pt-4 rounded-lg leading-normal overflow-hidden">
+              bg-gray-800  pb-6 pt-4 rounded-lg leading-normal h-full overflow-hidden">
         <div class="top mb-2 flex">
           <div class="h-3 w-3 bg-red-500 rounded-full"></div>
           <div class="ml-2 h-3 w-3 bg-orange-300 rounded-full"></div>
