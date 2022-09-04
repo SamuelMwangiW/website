9579624c94817566353c745388ade5c1d63956d6
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-09-04 07:44:01 +0300

    Restrict max width

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="flex items-center justify-center align-middle h-screen">
-    <div class="w-[480px] h-[240px]">
+    <div class="w-[480px] max-w-[80%] h-[240px]">
       <div class="coding inverse-toggle px-5 pt-4 shadow-lg text-gray-100 text-sm font-mono subpixel-antialiased
               bg-gray-800  pb-6 pt-4 rounded-lg leading-normal h-full overflow-hidden">
         <div class="top mb-2 flex">
